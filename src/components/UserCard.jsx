174043fac9259fdb74338707e6ece0bfd595fe0e
Nani-Prabhakar <nanibramhasamudram@gmail.com>
2025-04-09 15:47:51 +0530

    feat:edit profile + toast messages

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -4,25 +4,29 @@ const UserCard = ({user}) => {
     //console.log(user)
     const {firstName,age,lastName,photoUrl,gender,about}=user;
   return (
-    <div className='flex justify-center my-12'>
-        <div className=" rounded-md card bg-base-300 w-96 shadow-sm">
-        <figure>
-            <img
-            src={photoUrl}
-            alt="Shoes" />
-        </figure>
-        <div className="card-body">
-            <h2 className="card-title">{firstName+" "+lastName}</h2>
-            {
-              age && gender &&  <p>{gender +" "+"("+{age}+")"}</p>
-            }
-            <p>{about}</p>
-            <div className=" flex card-actions justify-center">
-            <button className="btn btn-secondary">ignore</button>
-            <button className="btn btn-primary">interested</button>
-            </div>
+    <div className="card bg-base-300 w-96 shadow-xl">
+      <figure>
+        <img src={photoUrl} alt="photo" />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">{firstName + " " + lastName}</h2>
+        {age && gender && <p>{age + ", " + gender}</p>}
+        <p>{about}</p>
+        <div className="card-actions justify-center my-4">
+          <button
+            className="btn btn-primary"
+            
+          >
+            Ignore
+          </button>
+          <button
+            className="btn btn-secondary"
+            
+          >
+            Interested
+          </button>
         </div>
-        </div>
+      </div>
     </div>
     
   )
